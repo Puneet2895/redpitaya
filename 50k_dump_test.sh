b90359ae0073f1dcde21a1dea3c30aeed2f060d2
Manoj Gulati <er.manojgulati@gmail.com>
2015-05-10 00:41:56 +0530

    Updated Test Script

--- 50k_dump_test.sh
+++ 50k_dump_test.sh
@@ -4,17 +4,24 @@ echo $start
 #dumps 100 files
 
 a=1
-while [ "$a" -lt 101 ]  
+while [ "$a" -lt 20001 ]  
 do 
- touch /mnt/usb/Omkar/"tmp_1_$a".csv
+ touch /mnt/storage/Dump1/"tmp_1_$a".csv
  a=`expr $a + 1`
 done
 
 b=1
-while [ "$b" -lt 101 ]  
+while [ "$b" -lt 20001 ]  
 do 
- touch /mnt/usb/Omkar/"tmp_2_$b".csv
+ touch /mnt/storage/Dump2/"tmp_2_$b".csv
  b=`expr $b + 1`
+done
+
+c=1
+while [ "$c" -lt 10001 ]  
+do 
+ touch /mnt/storage/Dump3/"tmp_3_$c".csv
+ c=`expr $c + 1`
 done
 
 end=`date +%s`
